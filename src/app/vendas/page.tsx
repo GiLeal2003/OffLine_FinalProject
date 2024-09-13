e94967bb2070c4a16eddf7b4eec3c151269ea00f
GiLeal2003 <gileal2003@outlook.com>
2024-09-13 18:58:50 -0300

    Commit

--- src/app/vendas/page.tsx
+++ src/app/vendas/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './vendas.module.css';
 
 export default function Page() {
-    return <p className={styles.title}>Seja Bem-Vindo a Área de Vendas</p>;
+    return <p className={styles.title}>Seja Bem-Vindo à Área de Vendas</p>;
   }
 
   
